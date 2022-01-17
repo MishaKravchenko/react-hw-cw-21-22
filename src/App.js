8a02993d56552596f6c0326b744291b5694a1baf
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-01-17 22:10:55 +0200

    new-add2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./App.css"
 
+import Form from "./components/form/form";
+import Cars from "./components/cars/cars";
+
 const App = () => {
     return (
         <div>
-
+            <Form/>
+            <Cars/>
         </div>
     );
 };
