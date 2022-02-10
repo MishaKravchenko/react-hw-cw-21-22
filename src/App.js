a9067e0d7876ad3c830c8dd770506ba53fb8b2af
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-02-10 17:51:33 +0200

    new-cw5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./App.css"
+import Form from "./components/Form/Form";
+import Todos from "./components/Todos/Todos";
 
 const App = () => {
     return (
         <div>
-
+            <Form/>
+            <Todos/>
         </div>
     );
 };
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import css from "./todo.module.css"
+import {useDispatch} from "react-redux";
+import {changeStatus, deleteTodo} from "../../store";
+
+const Todo = ({todo: {id, name, status}}) => {
+    const dispatch = useDispatch();
+    return (
+        <div className={css.Todo}>
+            <input type="checkbox" value={status} onChange={() => dispatch(changeStatus({id}))}/>
+            <div className={status ? css.completed : ""}>{name}</div>
+            <button onClick={() => dispatch(deleteTodo({id}))}>Delete</button>
+        </div>
+    );
+};
+
+export default Todo;
